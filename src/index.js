337fce796d267fc3a684ae935c12e91b1159c916
Gunel Huseynova <gorellik246@gmail.com>
2020-10-21 15:06:35 +0300

    refactor: change towelSort func

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (!matrix || !matrix.length) return [];
+    return matrix.reduce((ac, curArr, curIndex) =>
+        curIndex % 2 == 0
+            ? [...ac].concat([...curArr])
+            : [...ac].concat([...curArr].reverse())
+    );
+};
